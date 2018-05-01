96109603764fa9d402e84da1d689fb39f923b978
Wesley Parr <wxp9764@gmail.com>
2018-05-01 10:15:22 -0700

    add devices

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+import Constellation from './components/Constellation';
+import { randomIntBetween } from './helpers/random';
+
+import './App.css';
+
+class App extends Component {
+    render() {
+        const BORDER_SPACE = 50;
+        const viewport = {
+            width: window.innerWidth,
+            height: window.innerHeight
+        };
+
+        return (
+            <svg
+                className="constellation"
+                viewBox={ `0 0 ${viewport.width} ${viewport.height}` }
+            >
+                <Constellation
+                    starCoords={ [
+                        {
+                            x: (viewport.width - BORDER_SPACE)/2,
+                            y: (viewport.height - BORDER_SPACE)/2
+                        },
+                        {
+                            x: randomIntBetween(BORDER_SPACE, (viewport.width - BORDER_SPACE)),
+                            y: randomIntBetween(BORDER_SPACE, (viewport.height - BORDER_SPACE))
+                        },
+                        {
+                            x: randomIntBetween(BORDER_SPACE, (viewport.width - BORDER_SPACE)),
+                            y: randomIntBetween(BORDER_SPACE, (viewport.height - BORDER_SPACE))
+                        },
+                        {
+                            x: randomIntBetween(BORDER_SPACE, (viewport.width - BORDER_SPACE)),
+                            y: randomIntBetween(BORDER_SPACE, (viewport.height - BORDER_SPACE))
+                        },
+                        {
+                            x: randomIntBetween(BORDER_SPACE, (viewport.width - BORDER_SPACE)),
+                            y: randomIntBetween(BORDER_SPACE, (viewport.height - BORDER_SPACE))
+                        }
+                    ] }
+                />
+            </svg>
+        );
+    }
+}
+
+export default App;
